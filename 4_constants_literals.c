861315d0dfd66e8d57a228a189e8bda0c02a61d1
mohithadap-dotcom <mohit.hadap@gmail.com>
2026-02-18 22:58:34 +0530

    Create 4_constants_literals.c

---
+++ 4_constants_literals.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main() {
+    const int num = 100;
+    printf("Constant int: %d\n", num);
+    printf("Defined constant PI: %.2f\n", PI);
+    return 0;
+}
